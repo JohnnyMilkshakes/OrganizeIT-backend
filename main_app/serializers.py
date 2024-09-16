e0f2dc0592c1b3c53f7550b0cdedca2f404e07ea
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-16 13:25:29 -0400

    add __str__ method to Location and update __str__ method for Item

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -5,6 +5,7 @@ class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = '__all__'
+        read_only_fields = ('user',)
         
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
