1e34c4497796ca051705e48b4a578012022ece6b
wendy-silva <silvawendy0131@gmail.com>
2024-09-16 11:25:07 -0400

    create location model/serializer

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import Home, LocationList, LocationDetail
 from .views import Home
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
+  path('locations/', LocationList.as_view(), name='location-list'),
+  path('locations/<int:id>/', LocationDetail.as_view(), name='location-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Location
+from .serializers import LocationSerializer
 
 # Create your views here.
 from rest_framework.views import APIView
@@ -9,3 +12,12 @@ class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to the organize-it api home route!'}
     return Response(content)
+  
+class LocationList(generics.ListCreateAPIView):
+  queryset = Location.objects.all()
+  serializer_class = LocationSerializer
+  
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Location.objects.all()
+  serializer_class = LocationSerializer
+  lookup_field = 'id'
